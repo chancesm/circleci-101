5f9be3f80f5a01a302f77e635ff9c1cdfa6c10f4
Chance Murray <chance.s.murray@gmail.com>
2019-02-21 17:34:02 -0800

    add /test route

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ let app = require('express')();
 app.get('/', (req, res) => {
   res.send('hello world');
 });
+app.get('/test', (req, res) => {
+  res.send('hello test');
+});
 // add returned server to the app instance so that it can be closed in the tests :)
 app.server = app.listen(3000);
 
